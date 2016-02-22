f5ae42ea77635c782eafebb7b169f7217f9e3c88
Alex Curteman <alexander.curteman@gmail.com>
2016-02-22 11:46:54 -0800

    Added code to clip rasters, load raster objects, and generate output name.

--- projectFunctions.py
+++ projectFunctions.py
@@ -1,14 +1,51 @@
 # Functions for final project go here.
 
+# Import needed modules
+from arcpy import Clip_management, Exists
+
 # The first return value for each function should be a boolean value representing if the
 # function was successfull or not.
 
-# Load all rasters passed to the function
-# def loadRasters( rastDEM, rastHydro, rastLandcover):
-
 # Clip all rasters by the given boundaries
-# def clipRasters( rastDEM, rastHydro, rastLandcover):
+def clipRaster(inRaster, bounds):
+    
+    # Create file name for output raster
+    outRaster = inRaster + '_CLIP'
+    outLen = len(outRaster)
+    clipCount = 1
 
+    # Check if output filename exists already, and correct if so
+    validName = False
+    while not validName:
+
+        # If it does not exist, name is valid
+        if Exists(outRaster) ==0:
+            #print('Output filename: {0}'.format(outRaster))
+            validName = True
+
+        # Else add number to end of output name and check again
+        else:
+            print('File <{0}> already exists, incrementing file name.'.format(outRaster))
+            outRaster = outRaster[:outLen] + str(clipCount)
+            clipCount += 1
+
+    # Print out parameters to be used in Clip_management
+    #print('Attempting Clip with following parameters: \n    ' +
+          #'in_raster: {0}\n    rectange: {1}\n    out_raster: {2}'.format(
+              #inRaster, bounds, outRaster))
+
+    # Attempt to execute Clip_management
+    try:
+        Clip_management(inRaster, bounds, outRaster)
+        return True, outRaster
+
+    # If Clip_management failed:
+    except:
+        #print('Error with Clip_management tool')
+        #print('File probably exists, but was previously created incorrectly.')
+        #print('Check directory for <{0}> and consider removing.'.format(outRaster))
+        return False, None
+    
 # If landcover raster is present, have user input values for each landcover type
 # def getLandcoverValues( rastLandcover)    This function should return a list of values
 #                                                                               for each unique value in the landcover raster.
@@ -17,7 +54,29 @@
 # def genHydro( rastHydro):     This function should return a raster/shapefile conaining hydrology
 
 # If output file name not provided, generate output file name
-# def genOutputName( rastDEM):       This function should return a name for the output file
+def genOutputName():       #This function should return a name for the output file
+
+    # Create file name for output raster
+    outRaster = 'archPredictiveModel'
+    outLen = len(outRaster)
+    clipCount = 1
+
+    # Check if output filename exists already, and correct if so
+    validName = False
+    while not validName:
+
+        # If it does not exist, name is valid
+        if Exists(outRaster) ==0:
+            #print('Output filename: {0}'.format(outRaster))
+            validName = True
+
+        # Else add number to end of output name and check again
+        else:
+            print('File <{0}> already exists, incrementing file name.'.format(outRaster))
+            outRaster = outRaster[:outLen] + str(clipCount)
+            clipCount += 1
+
+    return outRaster
 
 # Perform raster calculations on given rasters
 # def calcModel( rastDEM, rastHydro, rastLandcover, numHydroBands, hydroBandDist,
--- projectScript.py
+++ projectScript.py
@@ -1,19 +1,52 @@
 # Main script for project goes in this file.
 
+# Import needed modules:
+from projectFuctions import *
+from arcpy import Raster
+
 # Define function name and arguments. Possible inputs: DEM, hydrology file, Landcover file,
 # bounding box to clip all rasters, number of hydrology bands, distance of hydrology bands,
 # number of slope bands, cut off points for slope bands, boolean value to include hydrology,
 # output file name,  list of landcover values.
-# def createArchModel( rastDEM, rastHydro = None, rastLandcover = None, boundary,
-#                                           numHydroBands = 3, hydroBandDist = 100, numSlopeBands = 4,
-#                                           slopeCutoff = [15, 30, 45], addHydro = False, outputRast = None,
-#                                           landcoverValues = None):
+def createArchModel( rastDEM, rastHydro = None, rastLandcover = None, boundary,
+                     numHydroBands = 3, hydroBandDist = 100, numSlopeBands = 4,
+                     slopeCutoff = [15, 30, 45], addHydro = False, outputRast = None,
+                     landcoverValues = None):
 
     # Clip all rasters by the given boundaries
-    # clipRasters( rastDEM, rastHydro, rastLandcover, boundary):
+    # Clipping the DEM
+    print('Attempting DEM clip.')
+    ok, clipDEM = clipRaster( rastDEM, rastHydro, rastLandcover, boundary):
 
-    # Load all rasters passed to the function
-    # loadRasters( rastDEM, rastHydro, rastLandcover):
+    #Check for errors, exit if needed
+    if not ok:
+        print('Error with DEM clip.')
+        return False, None
+
+    # If hydrology is given, clip
+    if not rastHydro == None:
+        print('Attempting Hydrology clip.')
+        ok, clipHydro = clipRaster( rastHydro, boundary)
+
+        # Check for errors, exit if needed
+        if not ok:
+            print('Error with Hydrology clip')
+            return False, None
+        
+    # If land cover is given, clip
+    if not rastLandcover == None:
+        print('Attempting Land cover clip.')
+        ok, clipLandcover = clipRaster( rastLandcover, boundary)
+
+        # Check for errors, exit if needed
+        if not ok:
+            print('Error with Land cover clip.')
+            return False, None
+
+    # Load all clipped rasters
+    DEM = Raster(clipDEM)
+    Hydro = Raster(clipHydro)
+    Landcover = Raster(clipLandcover)
 
     # If landcover raster is present, and no values passed, have user input values for each landcover type
     # landcoverValues = getLandcoverValues( rastLandcover)       This function should return a list of values
@@ -21,10 +54,13 @@
     #                                                                                                           raster.
 
     # If hydrology was not passed, but desired, generate from DEM
-    # rastHydro = genHydro( rastHydro):     This function should return a raster/shapefile conaining hydrology
+    if rastHydro == None and addHydro == True:
+        print('Generating Hydrology.')
+        rastHydro = genHydro( rastHydro):     #This function should return a raster/shapefile conaining hydrology
 
     # If output file name not provided, generate output file name
-    # outputRast = genOutputName( rastDEM):     This function should return a name for the output file
+    if outputRast == None:
+        outputRast = genOutputName():     #This function should return a name for the output file
 
     # Perform raster calculations on given rasters
     # outputRast = calcModel( rastDEM, rastHydro, rastLandcover, numHydroBands, hydroBandDist,
